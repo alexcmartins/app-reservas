983a63373702adc667245f83c558d7db4bea6508
Alex C. Martins <cantareiramartins@gmail.com>
2022-05-31 18:26:17 +0100

    Create pages/Control login, register and error

--- routes.js
+++ routes.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const route = express.Router();
+
 const homeController = require('./controllers/homeController');
 const menuController = require('./controllers/menuController');
 const menuPDFController = require('./controllers/menuPDFController');
+const error404Controller = require('./controllers/error404Controller');
+const loginController = require('./controllers/loginController');
+const registerController = require('./controllers/registerController');
 
 route.get('/', homeController.home);
 route.get('/menu', menuController.menu);
 route.get('/menuPDF', menuPDFController.menuPDF);
+route.get('/error404', error404Controller.error404);
+route.get('/login', loginController.login);
+route.get('/register', registerController.register);
 
 module.exports = route;
